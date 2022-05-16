68fffda14ddf9287a3d9b542729de531f14235a5
Martin-J-Larre <martinjlarre@gmail.com>
2022-05-15 22:33:16 -0400

    #5 contact page/form started

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,19 +1,36 @@
-import React from 'react';
-import './contact.css'
+import React from "react";
+import "./contact.css";
 
 const Contact = () => {
-  return (
-    <div>
-      <h1>Contact page soon here</h1>
-      <h1>Contact page soon here</h1>
-      <h1>Contact page soon here</h1>
-      <h1>Contact page soon here</h1>
-      <h1>Contact page soon here</h1>
-      <h1>Contact page soon here</h1>
-      <h1>Contact page soon here</h1>
-      <h1>Contact page soon here</h1>
-    </div>
-  )
-}
+    return (
+        <section>
+          <h1>Contact</h1>
+          <div className="contact-container">
+            <form>
+                <div className="name-group">
+                    <input
+                        type="text"
+                        name="user_name"
+                        placeholder="First name"
+                    />
+                    <input
+                        type="text"
+                        name="user_name"
+                        placeholder="Last name"
+                    />
+                </div>
+                <input
+                    type="email"
+                    name="user_email"
+                    placeholder="Subject"
+                    className="email"
+                />
+                <textarea name="message" placeholder="Message" />
+                <input type="submit" value="Send" />
+            </form>
+            </div>
+        </section>
+    );
+};
 
-export default Contact
+export default Contact;
