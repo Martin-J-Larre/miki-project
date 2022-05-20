1ae72abbfdb481817306c27236abe2be51e8645f
Martin-J-Larre <martinjlarre@gmail.com>
2022-05-19 23:21:00 -0400

    #7 layout fixed with Grid started

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -4,30 +4,42 @@ import styles from "./contact.module.css";
 const Contact = () => {
     return (
         <section>
-          <h1>Contact</h1>
-          <div className={styles.contactContainer}>
-            <form>
-                <div className={styles.nameGroup}>
+            <h1>Contact</h1>
+            <div className={styles.contactContainer}>
+                <form className={styles.form}>
+                    
+                        <input
+                            type="text"
+                            name="user_name"
+                            placeholder="First name"
+                            className={styles.firstNameInput}
+                        />
+                        <input
+                            type="text"
+                            name="user_name"
+                            placeholder="Last name"
+                            className={styles.lastNameInput}
+                        />
+                    
                     <input
-                        type="text"
-                        name="user_name"
-                        placeholder="First name"
+                        type="email"
+                        name="user_email"
+                        placeholder="Subject"
+                        className={styles.InputEmail}
                     />
-                    <input
-                        type="text"
-                        name="user_name"
-                        placeholder="Last name"
-                    />
-                </div>
-                <input
-                    type="email"
-                    name="user_email"
-                    placeholder="Subject"
-                    className={styles.email}
-                />
-                <textarea name="message" placeholder="Message" />
-                <input type="submit" value="Send" />
-            </form>
+                    <textarea 
+                        name="message" 
+                        placeholder="Message"
+                        className={styles.textArea}
+                        />
+                    <div className={styles.buttonSubmit}>
+                        <input 
+                            type="submit" 
+                            value="Send"
+                            />
+                    </div>    
+        
+                </form>
             </div>
         </section>
     );
