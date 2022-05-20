a521de32d3bdc92ed7a625765e8d74a9e0eb56f6
Martin-J-Larre <martinjlarre@gmail.com>
2022-05-20 00:42:38 -0400

    #8 EmailJS added

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,29 +1,62 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import styles from "./contact.module.css";
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const SERVICE_ID = process.env.REACT_APP_SERVICE_ID
+    const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID
+    const PUBLIC_KEY= process.env.REACT_APP_PUBLIC_KEY
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                SERVICE_ID,
+                TEMPLATE_ID,
+                form.current,
+                PUBLIC_KEY
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                    console.log("Message sent");
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
     return (
         <section>
             <h1>Contact</h1>
             <div className={styles.contactContainer}>
-                <form className={styles.form}>
+                
+                <form 
+                    ref={form} 
+                    onSubmit={sendEmail}
+                    className={styles.form}
+                >
                     
                         <input
                             type="text"
-                            name="user_name"
+                            name="name"
                             placeholder="First name"
                             className={styles.firstNameInput}
                         />
                         <input
                             type="text"
-                            name="user_name"
+                            name="last_name"
                             placeholder="Last name"
                             className={styles.lastNameInput}
                         />
                     
                     <input
                         type="email"
-                        name="user_email"
+                        name="email"
                         placeholder="Subject"
                         className={styles.InputEmail}
                     />
